341330778bc58ba9c339874f9519747cb6d895b1
Ananya Pathak <noreplylunarva@gmail.com>
2026-09-15 15:30:48 +0530

    Time: 0 ms (100%), Space: 12.2 MB (48.95%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*,Node*>mp;
+    Node* cloneGraph(Node* node) {
+        if(node == nullptr){
+            return nullptr;
+        }
+        if(mp.find(node)!=mp.end()){
+            return mp[node];
+        }
+        Node* copy = new Node(node->val);
+        mp[node]=copy;
+        for(Node* neighbor:node->neighbors){
+            copy->neighbors.push_back(cloneGraph(neighbor));
+        }
+    return copy;       
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
