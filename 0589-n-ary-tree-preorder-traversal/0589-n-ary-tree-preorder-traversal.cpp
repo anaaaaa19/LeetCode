fffeb5dc2c37bcd43c6d4744450df3c8fc948d08
Ananya Pathak <noreplylunarva@gmail.com>
2026-09-15 15:14:21 +0530

    Time: 7 ms (95.12%), Space: 18.6 MB (7.69%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> ans;
+        if(root == nullptr){
+            return ans;
+        }
+        ans.push_back(root->val);
+        for(Node* child : root->children){
+            vector<int> s = preorder(child);
+            ans.insert(ans.end(),s.begin(),s.end());
+        }
+        return ans;   
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
